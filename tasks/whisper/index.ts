94cae077c2d58188fb79483f64d3264bc9bad10e
Tomasz Jeniec <tomasz.jeniec@novacura.com>
2023-11-14 19:06:06 +0100

    extract url methods to separate class

--- tasks/whisper/index.ts
+++ tasks/whisper/index.ts
@@ -1,23 +1,9 @@
 import { execute } from "../../task-provider";
 import { WhisperInput } from "./WhisperInput";
 import { OpenAI, toFile } from "openai";
-import fs from "fs"
+import { extractUrl } from "../../tools/urlTools";
 
 const taskName = "whisper"
-
-const extractUrl = (input: string): string => {
-    const urlRegex = /(https?:\/\/[^\s]+)/;
-
-    const extractedUrl = input.match(urlRegex);
-
-    if (extractedUrl) {
-        const url = extractedUrl[0];
-        return url;
-    } else {
-        console.error("No URL found in the input string.");
-        process.exit(1)
-    }
-}
 
 const filename = (url: string): string => {
     const parts = url.split('/')
---
+++ tools/urlTools.ts
@@ -0,0 +1,13 @@
+export const extractUrl = (input: string): string => {
+    const urlRegex = /(https?:\/\/[^\s]+)/;
+
+    const extractedUrl = input.match(urlRegex);
+
+    if (extractedUrl) {
+        const url = extractedUrl[0];
+        return url;
+    } else {
+        throw Error("No URL found in the input string.");
+    }
+}
+
